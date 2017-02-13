6879d4773de0f4731a4144df311ecb3a8c56d722
Jun Mark Torii <j.torii4@gmail.com>
2017-02-12 22:42:10 -0500

    Better use of inheritance?

---
+++ MarkBreakdown/View Controllers/Base View Controllers/BaseTableViewController.swift
@@ -0,0 +1,84 @@
+//
+//  BaseTableViewController.swift
+//  MarkBreakdown
+//
+//  Created by Jun Torii on 2017-02-12.
+//  Copyright © 2017 Mark Torii. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+import StatefulViewController
+
+class BaseTableViewController: UIViewController {
+    
+    var tableView: UITableView!
+    
+    var disposeBag = DisposeBag()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupNavigationBar()
+        setupEmptyView()
+        setupView()
+        setupCellConfiguration()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        setupInitialViewState()
+    }
+    
+    
+    // MARK: Setup
+    
+    func setupNavigationBar() {
+        let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(TermViewController.addButtonTapped))
+        let editButton = self.editButtonItem
+        
+        navigationItem.rightBarButtonItems = [addButton, editButton]
+    }
+    
+    func setupEmptyView() {
+        // Nothing to do here
+    }
+    
+    func setupView() {
+        tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.estimatedRowHeight = 70.0;
+        tableView.tableFooterView = UIView(frame: CGRect.zero)
+        view.addSubview(tableView)
+        
+        setupContraints()
+    }
+    
+    func setupCellConfiguration() {
+        // Nothing to do here
+    }
+    
+    private func setupContraints() {
+        var layoutContraints = [NSLayoutConstraint]()
+        let views: [String: AnyObject] = ["tableView": tableView]
+        layoutContraints.append(contentsOf:(NSLayoutConstraint.constraints(withVisualFormat: "H:|[tableView]|", metrics: nil, views: views)))
+        layoutContraints.append(contentsOf:(NSLayoutConstraint.constraints(withVisualFormat: "V:|[tableView]|", metrics: nil, views: views)))
+        NSLayoutConstraint.activate(layoutContraints)
+    }
+    
+    
+    // MARK: Navigation bar button actions
+    
+    func addButtonTapped(sender: UIButton) {
+        // Nothing to do here
+    }
+}
+
+// MARK: StatefulViewController delegates
+extension BaseTableViewController: StatefulViewController {
+    func hasContent() -> Bool {
+        return false;
+    }
+}
--- MarkBreakdown/View Controllers/Term Tab/TermViewController.swift
+++ MarkBreakdown/View Controllers/Term Tab/TermViewController.swift
@@ -11,57 +11,19 @@ import RxSwift
 import RxCocoa
 import StatefulViewController
 
-class TermViewController: UIViewController {
-    
-    fileprivate var tableView: UITableView!
-    
-    fileprivate let disposeBag = DisposeBag()
+class TermViewController: BaseTableViewController {
 
-    
-    // MARK: View Controller lifecycle
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        setupNavigationBar()
-        setupEmptyView()
-        setupView()
-        setupCellConfiguration()
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        setupInitialViewState()
-    }
-    
-    
-    // MARK: Setup
-    
-    private func setupNavigationBar() {
-        let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(TermViewController.addButtonTapped))
-        let editButton = self.editButtonItem
-        
-        navigationItem.rightBarButtonItems = [addButton, editButton]
-    }
-    
-    private func setupEmptyView() {
+    override func setupEmptyView() {
         emptyView = DefaultEmptyView(title: "You Have No Terms", subTitle: "Tap the + button to add a new term")
     }
-
-    private func setupView() {
+    
+    override func setupView() {
         title = "Marks"
         
-        tableView = UITableView()
-        tableView.translatesAutoresizingMaskIntoConstraints = false
-        tableView.rowHeight = UITableViewAutomaticDimension
-        tableView.estimatedRowHeight = 70.0;
-        tableView.tableFooterView = UIView(frame: CGRect.zero)
-        view.addSubview(tableView)
-        
-        setupContraints()
+        super.setupView()
     }
     
-    private func setupCellConfiguration() {
+    override func setupCellConfiguration() {
         let testTerm = Term(termName: "Test Term", year: 2017, courses: [])
         MasterDataSource.sharedInstance.terms.value.append(testTerm)
         
@@ -72,18 +34,7 @@ class TermViewController: UIViewController {
         }.addDisposableTo(disposeBag)
     }
     
-    private func setupContraints() {
-        var layoutContraints = [NSLayoutConstraint]()
-        let views: [String: AnyObject] = ["tableView": tableView]
-        layoutContraints.append(contentsOf:(NSLayoutConstraint.constraints(withVisualFormat: "H:|[tableView]|", metrics: nil, views: views)))
-        layoutContraints.append(contentsOf:(NSLayoutConstraint.constraints(withVisualFormat: "V:|[tableView]|", metrics: nil, views: views)))
-        NSLayoutConstraint.activate(layoutContraints)
-    }
-    
-    
-    // MARK: Navigation bar button actions
-    
-    func addButtonTapped(sender: UIButton) {
+    override func addButtonTapped(sender: UIButton) {
         print("Add Button pressed")
         let addTermViewController = AddTermViewController()
         navigationController?.pushViewController(addTermViewController, animated: true)
@@ -92,26 +43,9 @@ class TermViewController: UIViewController {
 
 
 // MARK: StatefulViewController delegates
-extension TermViewController: StatefulViewController {
-    func hasContent() -> Bool {
+extension TermViewController  {
+    override func hasContent() -> Bool {
         return MasterDataSource.sharedInstance.terms.value.count > 0
     }
 }
-
-
-// MARK: UITableViewDelegate delegates
-//extension TermViewController: UITableViewDelegate {
-//    override func setEditing(_ editing: Bool, animated: Bool) {
-//        super.setEditing(editing, animated: animated)
-//        tableView.isEditing = editing
-//        
-//        if editing {
-//            // Nothing to do here?
-//        }
-//    }
-//    
-//    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-//        return 60
-//    }
-//}
 
